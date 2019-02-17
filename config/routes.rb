432f2b03816917b40bf1db6d9856922ebd9a049f
Ian Gigliotti <ian@sala33.com.br>
2019-02-17 10:53:18 -0300

    link to done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
+
   get 'about', to: 'pages#about'
 
   get 'albums', to: 'albums#index'
 
   post 'albums', to: 'albums#create'
 
-  get 'albums/:id', to: 'albums#show'
+  get 'albums/:id', to: 'albums#show', as: :album
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
